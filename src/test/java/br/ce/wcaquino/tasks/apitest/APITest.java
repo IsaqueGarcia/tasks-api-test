bd37777e2ff783e35113933b9db7815f14afc519
M283306 <isaquegarcia@sisconsultoria.com.br>
2022-05-31 11:43:06 -0300

    Ajustes nos testes

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -8,7 +8,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 public class APITest {
-	
+
 	@BeforeClass
 	public static void setup() {
 		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
@@ -16,63 +16,32 @@ public class APITest {
 
 	@Test
 	public void deveRetornarTarefas() {
-		RestAssured.given()
-		.when()
-			.get("/todo")
-		.then()
-			.statusCode(200)
-		;
+		RestAssured.given().when().get("/todo").then().statusCode(200);
 	}
-	
+
 	@Test
 	public void deveAdicionarTarefaComSucesso() {
-		RestAssured.given()
-			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2022-12-30\" }")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-			.statusCode(201)
-		;
+		RestAssured.given().body("{ \"task\": \"Teste via API\", \"dueDate\": \"2022-12-30\" }")
+				.contentType(ContentType.JSON).when().post("/todo").then().statusCode(201);
 	}
-	
+
 	@Test
 	public void naoDeveAdicionarTarefaInvalida() {
-		RestAssured.given()
-			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2010-12-30\" }")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-			.statusCode(400)
-			.body("message", CoreMatchers.is("Due date must not be in past"))
-		;
+		RestAssured.given().body("{ \"task\": \"Teste via API\", \"dueDate\": \"2010-12-30\" }")
+				.contentType(ContentType.JSON).when().post("/todo").then().statusCode(400)
+				.body("message", CoreMatchers.is("Due date must not be in past"));
 	}
-	
-	
+
 	@Test
 	public void deveRemoverTarefaComSucesso() {
-		//inserir
-		Integer id = RestAssured.given()
-			.body("{ \"task\": \"Tarefa para remoção\", \"dueDate\": \"2020-12-30\" }")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-//			.log().all()
-			.statusCode(201)
-			.extract().path("id")
-		;
-		
+		// inserir
+		Integer id = RestAssured.given().body("{ \"task\": \"Teste via API\", \"dueDate\": \"2022-12-30\" }")
+				.contentType(ContentType.JSON).when().post("/todo").then().statusCode(201).extract().path("id");
+
 		System.out.println(id);
-		
-		//remover
-		RestAssured.given()
-		.when()
-			.delete("/todo/"+id)
-		.then()
-			.statusCode(204)
-		;
+
+		// remover
+		RestAssured.given().when().delete("/todo/" + id).then().statusCode(204);
 	}
-	
+
 }
